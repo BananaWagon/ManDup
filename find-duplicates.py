ac6b9af27f16c45d242d5169a509b76cefc23e8d
Raymond Huff <rehuff83@gmail.com>
2016-07-17 00:41:51 -0500

    refactored based on gist given by /u/cscanlin

--- find-duplicates.py
+++ find-duplicates.py
@@ -30,17 +30,22 @@ class DuplicateFinder(object):
         self.path = path
         self.destination = destination
         self.option = option
-        self.file_index = []
+        self.current_file = ''
+        self.file_index = set()
         self.file_dup_index = []
         self.dir_dup_index =[]
         self.moved = 0
         self.not_moved = 0
+        self.deleded = 0
         self.total_size_index = 0
         self.total_size_moved = 0
-        self.total_size_moved = 0
+        self.total_size_dup = 0
         self.total_size_deleted= 0
         
-   def locate_files(self, path):     
+        
+   def locate_files(self):
+        """Parses all files in path including sub directories."""
+        
         for (dirname, dirs, files) in os.walk(self.path):
             for filename in files:
                 # Ignore files in this set
@@ -48,63 +53,108 @@ class DuplicateFinder(object):
                     # From what I can tell yield gives next in list without
                     # using a lot of memory.
                     yield join(dirname, filename)
-                    
-""" Continue refactor here. """                    
-    def add
-        # List/Move/Delete file if file is a duplicate
-        elif str(filename) in first_instance:
-            if not filename in file_duplist:                    
-                if not dirname in dir_duplist:
-                    dir_duplist.append(dirname)
-                    print("\nFolder: " + dirname)
-                file_duplist.append(filename)
-                
-                # List all duplicate files in the given path
-                if option == "-list":
-                    total_size += os.path.getsize(the_file)
-                    print("    " + filename)
-                    
-                # Move all duplicate files in given path to destination
+            
+            
+    def add_to_dup_index(self):
+        """Adds file names and directory names to a list."""
+        
+        self.total_size_dup += getsize(basename(self.current_file))
+        self.file_dup_index.append(basename(self.current_file))
+        if dirname not in self.dir_dup_index:
+            self.dir_dup_index.append(dirname(self.current_file))
+            print("\nFolder: {0}".format(dirname(self.current_file)))
+            
+            
+    def move_file(self):
+        """Moves all duplicate files in given path to destination"""
+        
                 # TODO: Make option to choose destination.
                 #       Perserve the directory structure when moving. 
-                #       Delete empty folders left behind. 
-                if option == "-move":
-                    try:
-                        print("Moving: " + filename, end=" ... ")
-                        shutil.move(the_file, destination)
-                        print("-=MOVED=-")
-                    except Exception as e:
-                        not_moved += 1
-                        # TODO: Validate file exists error
-                        print("!!! File already exists !!!")
+                #       Delete empty folders left behind.
+        if args.move:
+            try:
+                print("Moving: {0}".format(self.current_file), end=" ... ")
+                shutil.move(self.current_file, self.destination)
+                print("-=MOVED=-")
+            except Exception as e:
+                print(e) # print error string to catch specific exceptions
+                self.not_moved += 1
+                # TODO: Validate file exists error
+                print("!!! File already exists !!!")
+        
+    
+    def delete_file(self):
+        """Deletes all duplicate files"""
+        
+        if args.delete:
+            try:
+                print("Deleting: {0}".format(basename(self.current_file, end=" ... ")))
+                # TODO: os.remove does not reliably delete a file. Find out why.
+                os.remove(self.current_file)
+                print("-=DELETED=-")
+            except Exception as e:
+                print(e) # print error string to catch specific exeptions 
+                         # do something for specific error
+    
+    
+    def run(self):
+        """List/Move/Delete files if file is a duplicate."""
+        
+        for self.current_file in self.locate_files():
+            if basename(self.current_file) in self.file_index:
+                self.total_size += getsize(self.current_file)
+                self.add_to_dup_index()
+            
+                if args.list:
+                    print("    {0}".format(self.current_file))
                 
-                # Delete all duplicate files in given path
-                if option == "-delete":
-                    try:
-                        print("Deleting: " + filename, end=" ... ")
-                        os.remove(the_file)
-                        print("-=DELETED=-")
-                    except Exception as e:
-                        print(e)
+                elif args.move:
+                    self.moved =+ 1
+                    self.move_file()
+            
+                elif args.delete:
+                    self.deleted += 1
+                    # sefl.total_size_deleted += getsize(self.current_file)
+                    self.delete_file()
                 
-        # Add file name to set and list if not in list
-        if not filename in file_list:
-            first_instance.add(str(filename))
-            file_list.append(filename)
+            if basename(self.current_file) not in self.file_index:
+                self.file_index.add(basename(self.current_file))
 
-# Output when finished. 
-# TODO: Put in function and use a text wrapper.
-#       Change output based on option argv        
-print("\n..........................................")
+                
+    def stats(self):
+        """Formats program metrics and prints to screen."""
+        
+        stats = """
+            ..........................................
+            {len_seen_files} total files on drive.
+            {len_dir_dup_index} directories have duplicates.
+            {len_file_duplist} duplicate files./tSize:  {total_size_indexed}
+            """.format(
+            len_seen_files = len(self.file_index),
+            len_dir_duplist = len(self.dir_dup_index),
+            len_file_dup_index = len(self.file_dup_index),
+            len_total_size_dup = int(self.total_size_dup / 1000000000),
+        )
+        
+        if args.move:
+            stats += """
+            {self.moved} duplicate files moved to:\t{self.destination}
+            {self.not_moved} files not moved.""".format(self=self)
+            
+        if args.delete:
+            stats += """
+            {self.deleted} duplicate files deleted from:\t{self.path}
+            """.format(self=self)
+            
+        return stats
+        
+    
+out_path = os.path.join(dirname(os.path(__file__)), 'out_folder')
 
-print("\n" + str(len(first_instance)) + " total files on drive.")
-print(str(len(dir_duplist)) + " directories have duplicates.")
-print(str(len(file_duplist)) + " duplicate files.\t" + "Size: " + str(int(total_size / 1000000000)))
-
-if option == "-move":
-    print(str(not_moved) + " duplicate files moved to:\t" + str(destination))
-
-
+# args.??? parser does not give a single option. Find out how to pass option given. 
+df = DuplicateFinder(args.path, args.???, destination = out_path)
+df.run()
+print(df.stats())
 
 
 
